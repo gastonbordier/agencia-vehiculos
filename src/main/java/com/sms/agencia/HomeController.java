ec7c060c1e5a95cd96a420154d2450424c5f0212
gastonbordier <68036343+gastonbordier@users.noreply.github.com>
2022-06-15 10:01:16 -0300

    vista de motocicletas funcionando

--- src/main/java/com/sms/agencia/HomeController.java
+++ src/main/java/com/sms/agencia/HomeController.java
@@ -10,7 +10,7 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		return "index.html";
+		return "home.html";
 	}
 
 }
--- src/main/java/com/sms/agencia/MotoController.java
+++ src/main/java/com/sms/agencia/MotoController.java
@@ -3,10 +3,15 @@ package com.sms.agencia;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,28 +24,26 @@ import com.sms.agencia.dataMotocicletas.MotocicletaRepository;
 @RequestMapping("/motocicletas")
 public class MotoController {
 	
+	private static Logger LOG = LoggerFactory.getLogger(AgenciaApplication.class);
+
 	@Autowired
 	private MotocicletaRepository repository;
 
 	@GetMapping
-	public String getListadoMotos(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		
+	public String getListadoMotos(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+			Model model) {
 		List<Motocicleta> lista = repository.findAll();
 		model.addAttribute("lista", lista);
-		
 		return "motocicletas.html";
 	}
-	
+
 	@PostMapping
-	public String setMotos(Model model) {
+	public String setMotos(@ModelAttribute(name = "nombre")String nombre, Model model) {
+		
+		LOG.info("Nombre = "+nombre);
 		List<Motocicleta> lista = repository.findAll();
-		lista.add(new Motocicleta("aaa", "aaa", "aaa", 0, 0, 0));
 		model.addAttribute("lista", lista);
-		
-		
-		
 		return "motocicletas.html";
 	}
 
 }
-
